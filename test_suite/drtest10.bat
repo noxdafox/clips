776d983a072b4e4460d9bf585434b6fb8506a0c5
garydriley <griley@clipsrules.net>
2018-12-17 21:05:04 +0000

    Fix for SourceForge Ticket #49

--- test_suite/drtest10.bat
+++ test_suite/drtest10.bat
@@ -569,3 +569,29 @@
 (run 1)
 (unwatch all)
 (clear)
+(deftemplate A (slot val))
+(deftemplate B)
+(deftemplate C)
+(deftemplate D)
+
+(deffacts infinite_setup
+  (A (val 1)))
+
+(defrule infinite_rule
+  (logical
+    (A (val ?val))
+    (not (and (B) (C)))
+    (test (eq ?val 1))
+    (not (D)))
+  =>
+  (assert (D)))
+
+(reset)
+(watch rules)
+(watch facts)
+(watch activations)
+(run 1)
+(unwatch all)
+(clear) ; SourceForge Ticket #49
+(::)
+(clear)
