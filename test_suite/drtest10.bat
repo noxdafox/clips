8adf3fe12119180de748e5725719e2b89c2af917
garydriley <griley@clipsrules.net>
2019-06-20 22:22:46 +0000

    63x 64x fix for SourceForge Ticket #56

--- test_suite/drtest10.bat
+++ test_suite/drtest10.bat
@@ -629,4 +629,24 @@
   
 (deffacts hellos
    (hello))
+(clear) ; Source Ticket #56
+
+(deftemplate maze
+   (multislot open-list)
+   (slot goal))
+
+(defrule test-1
+   (maze (open-list)
+         (goal ?g&nil))
+   =>)
+
+(defrule test-2
+   (maze (open-list) 
+         (goal ?g&:(eq ?g nil)))
+   =>)
+
+(defrule test-3
+   (maze (open-list) 
+         (goal ~nil))
+   =>)
 (clear)
