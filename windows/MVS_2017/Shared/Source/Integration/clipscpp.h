9bb384b4ab0308bec051ddf90f3d40234df4f041
garydriley <griley@clipsrules.net>
2019-11-14 23:13:53 +0000

    64x C++ wrapper compilation error/warning fixes

--- windows/MVS_2017/Shared/Source/Integration/clipscpp.h
+++ windows/MVS_2017/Shared/Source/Integration/clipscpp.h
@@ -102,9 +102,9 @@ class CLIPSCPPEnv
       void AppendToDribble(const char *);
       void PrintBanner();
       void PrintPrompt();
-      void CLIPSCPPEnv::GetFactScopes(std::unordered_map<unsigned long long,std::vector<bool>>&);
+      void GetFactScopes(std::unordered_map<unsigned long long,std::vector<bool> >&);
 	  std::vector<CLIPSCPPFactInstance> *GetFactList();
-      void CLIPSCPPEnv::GetInstanceScopes(std::unordered_map<unsigned long long,std::vector<bool>>&);
+      void GetInstanceScopes(std::unordered_map<unsigned long long,std::vector<bool> >&);
 	  std::vector<CLIPSCPPFactInstance> *GetInstanceList();
 	  std::vector<CLIPSCPPModule> *GetModuleList();
       CLIPSCPPFocusStack *GetFocusStack();
